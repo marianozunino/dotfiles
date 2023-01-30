2c42b5876ffddff42a7349dde313ef1f08162d39
Mariano Z <marianoz@posteo.net>
2023-01-29 21:52:25 -0300

    feat: update redis.sh

--- dot_bin/executable_redis.sh
+++ dot_bin/executable_redis.sh
@@ -6,10 +6,12 @@ bin=~/.bin/redis
 if [ ! -f $bin ]; then
 	echo "Downloading redis binary..."
   filename=$(curl -s https://api.github.com/repos/qishibo/AnotherRedisDesktopManager/releases/latest | jq -r '.assets[].name' | grep "AppImage")
-  fileurl=$(curl -s https://api.github.com/repos/qishibo/AnotherRedisDesktopManager/releases/latest | jq -r --arg filename "Another-Redis-Desktop-Manager.1.5.8.AppImage" '.assets[] | select(.name == $filename) | .browser_download_url')
+  fileurl=$(curl -s https://api.github.com/repos/qishibo/AnotherRedisDesktopManager/releases/latest | jq -r --arg filename $filename '.assets[] | select(.name == $filename) | .browser_download_url')
   wget -q -O $bin $fileurl
 	chmod +x $bin
 fi
 
 # execute leapp
 $bin -f -i $1 -o $2
+
+1189
