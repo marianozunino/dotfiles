5c8ed02af4331d485cfb475fa3e2d785ebf258e6
Mariano Z <marianoz@posteo.net>
2023-06-18 14:22:44 -0300

    feat: update sm executable

--- dot_bin/executable_sm.sh
+++ dot_bin/executable_sm.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 URL=$(curl https://www.sublimemerge.com/download_thanks?target=x64-tar#direct-downloads | grep -oP 'https://download.sublimetext.com/sublime_merge_build_\d+_x64.tar.xz' | head -n 1)
 VERSION=$(echo $URL | grep -oP '\d+' | head -n 1)
-FILENAME="sublime_merge_build_${VERSION}_x64.tar.xz"
+FILENAME=sublime_merge_build_"$VERSION"_x64.tar.xz
 bin=~/.bin/sm
+
+# set current working directory to ~/.bin
+
+cd ~/.bin
 
 # if FILENAME exists, then exit
 if [ ! -f $FILENAME ]; then
-	echo "Downloading Sublime Merge $VERSION"
+  notify-send -u normal "⏬ Sublime Merge" "Updating to version $VERSION"
   # wget only if file does not exist
   wget -nc $URL
   tar -xvf $FILENAME
