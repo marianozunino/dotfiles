1b32157855bda4bad51714d6793c0dac6a75a91a
Mariano Z. <marianoz@posteo.net>
2023-09-18 12:15:20 -0300

    chore: fix wacom and other shit

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -58,11 +58,14 @@ return {
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
+		config = function()
+			require("todo-comments").setup({
+				keywords = {
+					FUCK = { icon = "󰇷 ", color = "error" },
+					SHITTY = { icon = "󰇷 ", color = "error" },
+				},
+			})
+		end,
 	},
 	{
 		"barrett-ruth/live-server.nvim",
