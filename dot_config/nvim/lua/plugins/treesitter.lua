fa5bef17cf9fff6faf773bdd80262bab3eeaab5b
Mariano Z. <marianoz@posteo.net>
2023-10-03 11:01:29 -0300

    chore: add lsp for go templ

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -36,6 +36,17 @@ return {
 			},
 		})
 
+		local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		treesitter_parser_config.templ = {
+			install_info = {
+				url = "https://github.com/vrischmann/tree-sitter-templ.git",
+				files = { "src/parser.c", "src/scanner.c" },
+				branch = "master",
+			},
+		}
+
+		vim.treesitter.language.register("templ", "templ")
+
 		require("treesitter-context").setup({
 			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
 			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
